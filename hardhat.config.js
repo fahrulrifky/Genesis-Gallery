4fbe241a72d453d4c7d51ee681e34f697ad7e986
yuhuu-96 <0xyuhuu96@gmail.com>
2026-06-30 14:20:13 +0700

    chore: update deployed contract address in index.html

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,7 +5,7 @@ module.exports = {
   networks: {
     ritual: {
       url: "https://rpc.ritualfoundation.org", // RPC URL Ritual
-      accounts: [] // private key akan diisi di sini
+      accounts: ["f95e981d42521e02ef2ba9501879839615e4e042682c67e459c80f7cfd99799a"] // private key akan diisi di sini
     }
   }
 };
